9ea9e44940c80a9c9cec70e98e66505fab0f7747
Jimmy Hillis <jimmy@hillis.me>
2015-07-31 11:03:45 +0800

    Added support for class to style placeholder

--- src/angular-placeholder.js
+++ src/angular-placeholder.js
@@ -1,7 +1,7 @@
-/**
+/**!
  * @ngdoc directive
  * @name ngPlaceholder.directive:ngPlaceholder
- * @restrict A
+ * @restrict E
  *
  * @description
  * HTML5 placeholder polyfill to ensure all browsers will support helpful
@@ -10,47 +10,53 @@
  * @param {string=} ngPlaceholder Text to use as placeholder value when no
  *     user content has been entered. Generally example usage of field.
  */
-angular.module('ngPlaceholder', []).directive('ngPlaceholder', [function() {
-    // Check for HTML5 placeholder support.
-    // Thanks to Modernizr for exmaple (http://modernizr.com/)
-    var PLACEHOLDER_SUPPORT = (function () {
-        var input_element = document.createElement('input');
-        return 'placeholder' in input_element;
-    }());
-    var placeholderDirective = {
-        restrict: 'A',
-        link: function (scope, element, attr) {
-            var _placeholder = attr.ngPlaceholder || attr.placeholder;
-            var _form = element[0].form;
-            // If browser has HTML5 placeholder ensure it is set and leave
-            if (PLACEHOLDER_SUPPORT) {
-                element.attr('placeholder', _placeholder);
-                return;
-            }
-            // Without HTML5 placeholder support set the value of the
-            // input by default and blur, and unset on focus.
-            element.val(_placeholder);
-            element.bind('focus', function (e) {
-                if (element.val() === _placeholder) {
-                    element.val('');
+
+// Check for HTML5 placeholder support.
+// Thanks to Modernizr for exmaple (http://modernizr.com/)
+var PLACEHOLDER_SUPPORT = (function () {
+    var input_element = document.createElement('input');
+    return 'placeholder' in input_element;
+}());
+
+angular.module('ngPlaceholder', [])
+    .directive('placeholder', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                var _placeholder = attr.ngPlaceholder || attr.placeholder;
+                var _form = element[0].form;
+                // If browser has HTML5 placeholder ensure it is set and leave
+                if (PLACEHOLDER_SUPPORT) {
+                    return;
                 }
-            });
-            element.bind('blur', function (e) {
+                // Without HTML5 placeholder support set the value of the
+                // input by default and blur, and unset on focus.
                 if (element.val() === '') {
                     element.val(_placeholder);
+                    element.addClass('placeholder');
                 }
-            });
-            // If this element is part of a form ensure that the placeholder
-            // value is not sent to the server on submit
-            if (_form !== null) {
-                angular.element(_form).bind('submit', function (e) {
+                element.bind('focus', function (e) {
                     if (element.val() === _placeholder) {
                         element.val('');
+                        element.removeClass('placeholder');
                     }
-                    return true;
                 });
+                element.bind('blur', function (e) {
+                    if (element.val() === '') {
+                        element.addClass('placeholder');
+                        element.val(_placeholder);
+                    }
+                });
+                // If this element is part of a form ensure that the placeholder
+                // value is not sent to the server on submit
+                if (_form !== null) {
+                    angular.element(_form).bind('submit', function (e) {
+                        if (element.val() === _placeholder) {
+                            element.val('');
+                        }
+                        return true;
+                    });
+                }
             }
-        }
-    };
-    return placeholderDirective;
-}]);
+        };
+    }]);
